d5b404ead2a85673be4785242633829a42f3ceb3
alexyn26 <107084291+alexyn26@users.noreply.github.com>
2022-12-05 09:36:57 -0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -15,7 +15,6 @@ const server = new ApolloServer({
   resolvers,
   context: authMiddleware,
 });
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -25,6 +24,11 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.use(routes);
+
+app.get ('*',(req, res)=> {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
